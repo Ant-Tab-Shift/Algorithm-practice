a4c995525a22729b0bd76ed6e0d71c8860b9e8c8
Ant-Tab-Shift <Ant.Tab.Shift@gmail.com>
2025-08-03 14:14:46 +0500

    Solve leetcode problem nomber 356

---
+++ line-reflection-356/solution.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+const inf = 109
+
+type Point struct {
+	x, y int
+}
+
+func isReflected(points [][]int) bool {
+	pointSet := make(map[Point]struct{}, len(points))
+	minX, maxX := inf, -inf
+	for _, point := range points {
+		pointSet[Point{point[0], point[1]}] = struct{}{}
+		minX = min(minX, point[0])
+		maxX = max(maxX, point[0])
+	}
+	doubledLineXCoord := minX + maxX
+	for point := range pointSet {
+		reflectedPoint := Point{doubledLineXCoord - point.x, point.y}
+		if _, ok := pointSet[reflectedPoint]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(isReflected([][]int{{1, 1}, {-1, 1}}))
+	fmt.Println(!isReflected([][]int{{1, 1}, {-1, -1}}))
+}
