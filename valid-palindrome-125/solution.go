fcd2d4e72d0d0a920c9272dfb251df7c4f8e0bb9
Ant-Tab-Shift <Ant.Tab.Shift@gmail.com>
2025-08-05 14:43:10 +0500

    Resolved 125's leetcode problem

---
+++ valid-palindrome-125/solution.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func isAlphaNum(char byte) bool {
+	sym := rune(char)
+	return unicode.IsLetter(sym) || unicode.IsDigit(sym)
+}
+
+func lower(char byte) rune {
+	return unicode.ToLower(rune(char))
+}
+
+func isPalindrome(s string) bool {
+	leftPtr, rightPtr := 0, len(s)-1
+	for leftPtr < rightPtr {
+		for leftPtr < rightPtr && !isAlphaNum(s[leftPtr]) {
+			leftPtr++
+		}
+		for rightPtr > leftPtr && !isAlphaNum(s[rightPtr]) {
+			rightPtr--
+		}
+		if lower(s[leftPtr]) != lower(s[rightPtr]) {
+			return false
+		}
+		leftPtr++
+		rightPtr--
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(isPalindrome("."))
+	fmt.Println(isPalindrome("A man, a plan, a canal: Panama"))
+}
