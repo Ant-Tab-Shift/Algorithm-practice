5e94f2272be30416da7e9ce21b24d15c4c23b559
Ant-Tab-Shift <Ant.Tab.Shift@gmail.com>
2025-08-06 20:14:32 +0500

    Solved Leetcode problem n821

---
+++ LeetCode/shortest-to-char-821/solution.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func shortestToChar(s string, c byte) []int {
+	answer := make([]int, len(s))
+	prevOccur := -len(s)
+	for idx := range len(s) {
+		if s[idx] == c {
+			prevOccur = idx
+		}
+		answer[idx] = idx - prevOccur
+	}
+	for idx := len(s) - 2; idx >= 0; idx-- {
+		answer[idx] = min(answer[idx], answer[idx+1]+1)
+	}
+	return answer
+}
+
+func main() {
+	fmt.Println(shortestToChar("a", 'a'))     // [0]
+	fmt.Println(shortestToChar("ba", 'a'))    // [1 0]
+	fmt.Println(shortestToChar("abbab", 'a')) // [0 1 1 0 1]
+}
