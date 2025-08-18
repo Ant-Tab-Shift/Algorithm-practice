ec356257475ef74be14f840851c1af1b1c81370d
Ant-Tab-Shift <Ant.Tab.Shift@gmail.com>
2025-08-18 08:33:47 +0500

    Keep it simple stupid for Leetcode 22

--- LeetCode/generate-parentheses-22/solution.go
+++ LeetCode/generate-parentheses-22/solution.go
@@ -17,43 +17,27 @@ func New(n int) *ParenthesesStack {
 	}
 }
 
-func (ps *ParenthesesStack) Push(p byte) {
-	ps.stack = append(ps.stack, p)
-}
-
-func (ps *ParenthesesStack) Pop() {
-	ps.stack = ps.stack[:len(ps.stack)-1]
-}
-
-func (ps *ParenthesesStack) AppendResult() {
-	ps.result = append(ps.result, string(ps.stack))
-}
-
 func (ps *ParenthesesStack) Backtrack(opened, closed int) {
 	if opened+closed == cap(ps.stack) {
-		ps.AppendResult()
+		ps.result = append(ps.result, string(ps.stack))
 		return
 	}
 	if opened < cap(ps.stack)/2 {
-		ps.Push('(')
+		ps.stack = append(ps.stack, '(')
 		ps.Backtrack(opened+1, closed)
-		ps.Pop()
+		ps.stack = ps.stack[:len(ps.stack)-1]
 	}
 	if closed < opened {
-		ps.Push(')')
+		ps.stack = append(ps.stack, ')')
 		ps.Backtrack(opened, closed+1)
-		ps.Pop()
+		ps.stack = ps.stack[:len(ps.stack)-1]
 	}
-}
-
-func (ps ParenthesesStack) Result() []string {
-	return ps.result
 }
 
 func generateParenthesis(n int) []string {
 	ps := New(n)
 	ps.Backtrack(0, 0)
-	return ps.Result()
+	return ps.result
 }
 
 func main() {
